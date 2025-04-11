e896cd45d1f1e1654c938a7055f942fa1ed387a4
okondev2021 <sojapril16@gmail.com>
2025-04-12 00:48:01 +0100

    removed dexie

--- src/appwrite/database.js
+++ src/appwrite/database.js
@@ -37,26 +37,3 @@ collections.forEach((col) => {
     get: (id) => databases.getDocument(col.dbId, col.id, id),
   };
 });
-
-//Realtime connection
-client.subscribe(`databases.${dbId}.collections.*.documents`, (response) => {
-  console.log("Real-time DB event received:", response);
-
-  const event = response.events.find(
-    (e) =>
-      e.includes(".create") || e.includes(".update") || e.includes(".delete"),
-  );
-
-  const doc = response.payload;
-
-  if (event?.includes(".create")) {
-    console.log("New document created:", doc);
-    // store new data in IndexedDB
-  } else if (event?.includes(".update")) {
-    console.log("Document updated:", doc);
-    //store updated data in IndexedDB
-  } else if (event?.includes(".delete")) {
-    console.log("Document deleted:", doc);
-    // remove document from indexedDB
-  }
-});
